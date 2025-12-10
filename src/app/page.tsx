b37332bd14a3a731e3ae920c22982cef61ba0ca7
Emanuel Silva <emanuel_silva2004@hotmail.com>
2025-12-10 19:24:58 -0300

    ssg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-'use client'; // Isso avisa ao Next.js para rodar esse arquivo como React normal (Client Side)
+import Home from '@/views/Home';
 
-// IMPORTANTE: Confira se o caminho '@views/Home' está certo.
-// Se você renomeou a pasta 'pages' para 'views', e o arquivo é 'Home.tsx', deve ser algo assim:
-import HomeAntiga from '@/views/Home'; 
+export const dynamic = 'force-static';
 
 export default function Page() {
-  return <HomeAntiga />;
+  return <Home />;
 }
